63ec7a45fe4f695bb08fd9f611f2e53aa98bcd1f
CarlosAsrc <carlos.asrc@gmail.com>
2018-09-09 02:01:24 -0300

    Adiciona paredes internas. Sem validação: ainda pode haver áreas isoladas no labirinto.

--- src/Maze.java
+++ src/Maze.java
@@ -5,7 +5,7 @@ public class Maze {
 	
 	private String [][] maze = new String [10][10];
 	private Random random = new Random();
-	private String wall = "";
+	private String wallSide = "";
 //	Agente: A
 //	Parede: P
 //	Buraco: O
@@ -45,7 +45,7 @@ public class Maze {
 		
 		switch(side) {
 			case 0:
-				wall = "ACIMA";
+				wallSide = "ACIMA";
 				for(int i=0; i<maze[0].length; i++) {
 					if(exit==i) 
 						maze[0][i] = " S "; 
@@ -57,7 +57,7 @@ public class Maze {
 				}
 				break;
 			case 1:
-				wall = "ESQUERDA";
+				wallSide = "ESQUERDA";
 				for(int i=0; i<maze.length; i++) {
 					if(exit==i) 
 						maze[i][0] = " S "; 
@@ -69,7 +69,7 @@ public class Maze {
 				}
 				break;
 			case 2:
-				wall = "ABAIXO";
+				wallSide = "ABAIXO";
 				for(int i=0; i<maze[0].length; i++) {
 					if(exit==i) 
 						maze[maze[0].length-1][i] = " S "; 
@@ -82,7 +82,7 @@ public class Maze {
 				
 				break;
 			case 3:
-				wall = "DIREITA";
+				wallSide = "DIREITA";
 				for(int i=0; i<maze.length; i++) {
 					if(exit==i) 
 						maze[i][maze[0].length-1] = " S "; 
@@ -93,7 +93,45 @@ public class Maze {
 					}
 				}
 				break;
+			}
+		//Constrói os 4 muros internos:
+		int direction;
+		int initialLine, initialColumn;
+		for(int i=0; i<4; i++) {
+			direction = random.nextInt(2);
+			
+			switch(direction) {
+			case(0):
+				do{
+					initialLine = random.nextInt(10);
+					initialColumn = random.nextInt(6);
+				}while( ! (maze[initialLine][initialColumn].equals(" - ")
+						&& maze[initialLine][initialColumn+1].equals(" - ")
+						&& maze[initialLine][initialColumn+2].equals(" - ")
+						&& maze[initialLine][initialColumn+3].equals(" - ")
+						&& maze[initialLine][initialColumn+4].equals(" - "))
+				);
+				for(int j=0; j<5; j++) {
+					maze[initialLine][initialColumn+j] = " P ";
+				}
+				break;
+			case(1): 
+				do{
+					initialLine = random.nextInt(6);
+					initialColumn = random.nextInt(10);
+				}while( ! (maze[initialLine][initialColumn].equals(" - ")
+						&& maze[initialLine+1][initialColumn].equals(" - ")
+						&& maze[initialLine+2][initialColumn].equals(" - ")
+						&& maze[initialLine+3][initialColumn].equals(" - ")
+						&& maze[initialLine+4][initialColumn].equals(" - "))
+				);
+				for(int j=0; j<5; j++) {
+					maze[initialLine+j][initialColumn] = " P ";
+				}
+				break;
+			}
 				
+			
 		}
 	}
 	
