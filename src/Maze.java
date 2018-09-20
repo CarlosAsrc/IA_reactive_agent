0bc5fa776a4a91255148c54b77fe775154591ff9
CarlosAsrc <carlos.asrc@gmail.com>
2018-09-20 16:09:31 -0300

    Melhoras no print do labirinto.

--- src/Maze.java
+++ src/Maze.java
@@ -282,11 +282,13 @@ public class Maze {
 	}
 
 	public void printMaze() {
+		System.out.println("    |0|  |1|  |2|  |3|  |4|  |5|  |6|  |7|  |8|  |9|");
 		for (int i = 0; i < maze.length; i++) {
+			System.out.print("|"+i+"|");
 			for (int j = 0; j < maze[0].length; j++) {
 				System.out.print(maze[i][j]);
 			}
-			System.out.println();
+			System.out.println("\n");
 		}
 	}
 
