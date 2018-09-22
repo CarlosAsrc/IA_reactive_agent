61477b7084410f5f179422f531f3ca4f8ef84f86
larissafiorini <larissa.fiorini@hotmail.com>
2018-09-22 18:33:51 -0300

    Refactoring a star'

--- src/A_star.java
+++ src/A_star.java
@@ -17,62 +17,29 @@ public class A_star {
 	}
 
 	public ArrayList<State> run() {
-		/*
-		 * lista_aberta lista_fechada
-		 *
-		 * lista_aberta[0] = inicio //coloca vizinhos dela na lista aberta
-		 * lista_aberta.add inicio+1 inicio-1
-		 *
-		 * para cada vizinha, v� custo da vizinha at� o objetivo, custo da vizinha at� o
-		 * inicio(anterior+1), e soma desses dois q gera o custo final. Custo = n�mero
-		 * de casas
-		 *
-		 * casa atual ent�o vai p lista fechada
-		 *
-		 * ordena lista aberta em ordem crescente de custos
-		 *
-		 * ent�o vai p quem t� no topo da lista aberta (aquele com menor custo). Repete
-		 * passos anteriores. Obst�culos s�o ignorados na an�lise de vizinhos. O que j�
-		 * foi computado e j� ta na lista aberta n�o � analisado de novo quando �
-		 * vizinho novamente. Analisa apenas vizinhos livres(que n�o est�o em nenhuma
-		 * das listas).
-		 */
 
 		ArrayList<State> open_list = new ArrayList<State>();
 		ArrayList<State> closed_list = new ArrayList<State>();
 		ArrayList<State> finalPath = new ArrayList<State>();
-		ArrayList<State> path = new ArrayList<State>();
 
 		int[] current = new int[2];
 
+		// Posicao inicial do agente
 		double cg = calcHeuristic(this.inicio, this.objetivo);
 		double cf = 0 + cg;
 		State inicial = new State(this.inicio, cg, 0, cf, this.inicio);
-		int count = 0;
-
 		open_list.add(inicial);
-		// adiciona posicao inicial do agente como primeiro elemento do path final - testar
-		path.add(inicial);
 
-		State menor_vizinho = null;
-		double menor_custo_vizinho = 50000000000.0;
-
+		int count = 0;
+		
 		while (!open_list.isEmpty()) {
 
 			// pega primeiro elemento da lista aberta (aquele com menor custo)
 			current = open_list.get(0).getPosition();
 
-			// checa se posicao atual e a posicao do objetivo(bau ou saida)
+			// checa se posicao atual = posicao do objetivo (bau ou saida)
 			if ((current[0] == this.objetivo[0]) && (current[1] == this.objetivo[1])) {
-//				System.out.println("Achou final: " + this.objetivo[0] + "," + this.objetivo[1]);
-//				open_list.forEach(s-> {System.out.print(" "+s.getPosition()[0]+s.getPosition()[1]);});
-//				System.out.println();
-//				closed_list.forEach(s-> {System.out.print(" "+s.getPosition()[0]+","+s.getPosition()[1]);});
-//				System.out.println();
-//				path.forEach(s-> {System.out.print(" "+s.getPosition()[0]+s.getPosition()[1]);});
-//				System.out.println();
 				finalPath = reconstructPath(closed_list);
-				//findPath(searchByFatherPos(closed_list,current[0],current[1]).getPosition(), closed_list);
 				break;
 			}
 
@@ -80,16 +47,13 @@ public class A_star {
 			ArrayList<int[]> vizinhos = getVizinhos(current);
 
 			for (int[] v : vizinhos) {
-
+				
 				int cost_initial = count;
 				double cost_goal = calcHeuristic(current, this.objetivo);
 				double cost_final = cost_initial + cost_goal;
-
 				int[] pos_pai = current;
-				State vizinho = new State(pos_pai, cost_goal, cost_initial, cost_final, v);
 
-				if (cost_final < menor_custo_vizinho)
-					menor_vizinho = vizinho;
+				State vizinho = new State(pos_pai, cost_goal, cost_initial, cost_final, v);
 
 				boolean contains = false;
 
@@ -112,33 +76,16 @@ public class A_star {
 					open_list.add(vizinho);
 			}
 
-//			System.out.println("menor vizinho: ");
-//
-//			System.out.println(menor_vizinho.getPosition()[0] + "," + menor_vizinho.getPosition()[1]);
-//			System.out.println(menor_vizinho.getPos_pai()[0] + "," + menor_vizinho.getPos_pai()[1]);
-//
-//			System.out.println("Atual: " + open_list.get(0).getPosition()[0] + "," + open_list.get(0).getPosition()[1]);
-
-			State atual = open_list.get(0);
 			closed_list.add(open_list.get(0));
 			open_list.remove(open_list.get(0));
 
-
 			Collections.sort(open_list);
-
-			path.add(menor_vizinho);
-			// ou
-			//path.add(atual);
 
 			vizinhos.clear();
 
 			count++;
 		}
-		 
-//		for (State s :finalPath) {
-//	  	//	System.out.println("Posi��o pai" + s.getPos_pai()[0] + "," + s.getPos_pai()[1]);
-//			System.out.println("PATH: " + s.getPosition()[0] + "," + s.getPosition()[1] + " custo: " + s.getCost_final());
-//		}
+
 		return finalPath;
 	}
 
@@ -151,10 +98,10 @@ public class A_star {
 			position[0] = current[0] + 1;
 			position[1] = current[1];
 			if (ag.validRangePos(position[0], position[1])) {
-				if(this.maze[position[0]][position[1]] != null) {
-					if(this.maze[position[0]][position[1]].contains("O") ) {
-						if(ag.validPos(position[0]+1, position[1])) {
-							position[0] = position[0]+1;
+				if (this.maze[position[0]][position[1]] != null) {
+					if (this.maze[position[0]][position[1]].contains("O")) {
+						if (ag.validPos(position[0] + 1, position[1])) {
+							position[0] = position[0] + 1;
 							vizinhos.add(position);
 						}
 					} else {
@@ -169,10 +116,10 @@ public class A_star {
 			position[0] = current[0] - 1;
 			position[1] = current[1];
 			if (ag.validRangePos(position[0], position[1])) {
-				if(this.maze[position[0]][position[1]] != null) {
-					if(this.maze[position[0]][position[1]].contains("O") ) {
-						if(ag.validPos(position[0]-1, position[1])) {
-							position[0] = position[0]-1;
+				if (this.maze[position[0]][position[1]] != null) {
+					if (this.maze[position[0]][position[1]].contains("O")) {
+						if (ag.validPos(position[0] - 1, position[1])) {
+							position[0] = position[0] - 1;
 							vizinhos.add(position);
 						}
 					} else {
@@ -186,10 +133,10 @@ public class A_star {
 			position[0] = current[0];
 			position[1] = current[1] + 1;
 			if (ag.validRangePos(position[0], position[1])) {
-				if(this.maze[position[0]][position[1]] != null) {
-					if(this.maze[position[0]][position[1]].contains("O") ) {
-						if(ag.validPos(position[0], position[1]+1)) {
-							position[1] = position[1]+1;
+				if (this.maze[position[0]][position[1]] != null) {
+					if (this.maze[position[0]][position[1]].contains("O")) {
+						if (ag.validPos(position[0], position[1] + 1)) {
+							position[1] = position[1] + 1;
 							vizinhos.add(position);
 						}
 					} else {
@@ -203,10 +150,10 @@ public class A_star {
 			position[0] = current[0];
 			position[1] = current[1] - 1;
 			if (ag.validRangePos(position[0], position[1])) {
-				if(this.maze[position[0]][position[1]] != null) {
-					if(this.maze[position[0]][position[1]].contains("O") ) {
-						if(ag.validPos(position[0], position[1]-1)) {
-							position[1] = position[1]-1;
+				if (this.maze[position[0]][position[1]] != null) {
+					if (this.maze[position[0]][position[1]].contains("O")) {
+						if (ag.validPos(position[0], position[1] - 1)) {
+							position[1] = position[1] - 1;
 							vizinhos.add(position);
 						}
 					} else {
@@ -222,42 +169,37 @@ public class A_star {
 		return vizinhos;
 	}
 
-	private  ArrayList<State> reconstructPath(ArrayList<State> path){
-		 // começa com último elemento do path, que é o state "objetivo"
-		 State current = path.get(path.size() - 1);
+	private ArrayList<State> reconstructPath(ArrayList<State> path) {
+		// comeca com ultimo elemento do path, que e o state "objetivo"
+		State current = path.get(path.size() - 1);
 
-		 ArrayList<State> finalPath = new ArrayList<State>();
-		 finalPath.add(current);
-		 int i=0, attempts=0;
-		 while(i<calcHeuristic(this.inicio, this.objetivo) && attempts<50){
-			 for (State s : path) {
-				if(s.getPosition()[0] == current.getPos_pai()[0] && s.getPosition()[1] == current.getPos_pai()[1]){
+		ArrayList<State> finalPath = new ArrayList<State>();
+		finalPath.add(current);
+		int i = 0, attempts = 0;
+		while (i < calcHeuristic(this.inicio, this.objetivo) && attempts < 50) {
+			for (State s : path) {
+				if (s.getPosition()[0] == current.getPos_pai()[0] && s.getPosition()[1] == current.getPos_pai()[1]) {
 					// current vai pro state anterior o atual, p/ o nodo came_from
-					 current = s;
-					 finalPath.add(current);
-					 i++;
-					 //System.out.print(" - "+current.getPosition()[0]+current.getPosition()[1]);
-					 break;
+					current = s;
+					finalPath.add(current);
+					i++;
+					break;
 				}
 
-			 }
-			 attempts++;	
-			 path.remove(current);
-		 }
-		 Collections.reverse(finalPath);
-		 return finalPath;
+			}
+			attempts++;
+			path.remove(current);
+		}
+		Collections.reverse(finalPath);
+		return finalPath;
 
 	}
 
 	/*
-	 * Gera a funcao heuristica do estado corrente
+	 * Gera a funcao heuristica do estado atual
 	 *
 	 */
-//	public double calcHeuristic(int[] current, int[] goal) {
-//		double distance = Math.hypot(current[0] - goal[0], current[1] - goal[1]);
-//		return distance;
-//	}
-	public  int calcHeuristic(int[] current, int[] goal) {
+	public int calcHeuristic(int[] current, int[] goal) {
 		int distance = Math.abs(current[0] - goal[0]) + Math.abs(current[1] - goal[1]);
 		return distance;
 	}
